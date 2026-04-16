b36ba2962b40e9cbbdcb664db282d52e9a2e3936
farhad202526 <fa99.official@gmail.com>
2026-04-16 22:10:34 +0600

    fix banner text

--- src/Pages/Hompages/Banner.jsx
+++ src/Pages/Hompages/Banner.jsx
@@ -11,7 +11,7 @@ const Banner = () => {
                     <h1 className="text-5xl font-bold">Friends to keep close in your life</h1>
                     <p className="py-6 text-[#64748B]">
                         Your personal shelf of meaningful connections. Browse, tend, and nurture the
-                        <br />relationships that matter most. F.
+                        <br />relationships that matter most.
                     </p>
                     <button className="btn bg-[#244D3F] text-yellow-50"><CgMathPlus /> Add a Friend</button>
                 </div>
